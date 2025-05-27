5b38df4be86b274e2ae51b33b2854960673d155b
sinasll <sinasalah6@gmail.com>
2025-05-27 14:47:06 +0300

    msg

--- bot.js
+++ bot.js
@@ -6,18 +6,13 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 const app = express();
 
 // Handle /start command
-bot.start((ctx) => {
-  const welcomeMessage = `
-*Welcome to the BLACK Mining App*
-
-Press *Start Mining* to begin\. Mining will continue running in the background automatically\.
-
-You can also *boost your mining power* by submitting daily codes from other users\.
-  `;
-  
-  ctx.reply(welcomeMessage.trim(), { parse_mode: 'MarkdownV2' });
-});
-
+bot.telegram.sendMessage(chatId, 
+  '*Welcome to the BLACK Mining App*\n\n' +
+  'Press *Start Mining* to begin\\. Mining will continue running in the background automatically\\.\n\n' +
+  'You can also *boost your mining power* by submitting daily codes from other users\\.',
+  { parse_mode: 'MarkdownV2' }
+);
+s
 // Set up webhook
 app.use(bot.webhookCallback('/webhook'));
 bot.telegram.setWebhook(`${process.env.WEBHOOK_URL}/webhook`);
